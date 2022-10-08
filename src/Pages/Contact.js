a611320a900e3a79d64f1926b07a62533e4dab14
AliAbbasAsif <aliabbasasif89@gmail.com>
2022-10-08 22:11:56 +0500

    first commit

---
+++ src/Pages/Contact.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import '../App.css'
+function Contact() {
+  console.log("Helodjksnhkbjgdvnb");
+  return (
+    <div className="App">
+      <header className="App-header">
+
+        <h1>This is Contact Page</h1>
+      </header>
+
+    </div>
+  )
+
+}
+
+export default Contact
---
+++ src/Pages/Services.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import '../App.css'
+function Services() {
+  return (
+    <div className="App">
+  
+      <header className="App-header">
+
+        <h1>This is Service Page.</h1>
+      </header>
+
+    </div>
+   
+  )
+}
+
+export default Services
